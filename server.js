150529c8eaf55d226beacc3353d421453184465d
Zubair Ahmed <zubair1024@gmail.com>
2021-08-17 21:53:41 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require("express");
+const app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: "hello API" });
+});
+
+/**
+ * Here is the needed API service
+ */
+app.get("/api/:date?", (req, res) => {
+  let dateObj = new Date(
+    isNaN(Number(req.params.date)) ? req.params.date : Number(req.params.date)
+  );
+
+  if (isNaN(dateObj.getTime())) {
+    dateObj = new Date(0);
+  }
+
+  res.status(200).send({
+    unix: dateObj.getTime(),
+    utc: dateObj.toGMTString(),
+  });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
